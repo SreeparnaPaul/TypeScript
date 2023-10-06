9bdce6d3fdaf51e7b95225f73eba36222ce96307
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-10-06 16:56:16 +0530

    Arrays

---
+++ myArray.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superheros = [];
+// const heroPower: number []=[]
+//or we can write it like
+var heroPower = [];
+var allUsers = [];
+var MLmodels = [
+    [67, 78],
+    []
+];
+superheros.push("ironman");
+heroPower.push(23);
+allUsers.push({
+    name: "Sree",
+    isActive: true
+});
--- myArray.ts
+++ myArray.ts
@@ -22,3 +22,4 @@ allUsers.push({
     isActive:true
 }
 )
+export {}
