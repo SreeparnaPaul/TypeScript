1b25b10de8a81a8597058aaa6f7a08150201c706
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-10-09 11:34:51 +0530

    tuples

---
+++ myTupEnums.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = [1, "hc",];
+var tUser;
+tUser = ["hc", 131, true]; // order of array elements need to be correct in tuple
+var rgb = [255, 123, 112];
+var newUser = [112, "example@google.com"];
+newUser[1] = "hc.com";
