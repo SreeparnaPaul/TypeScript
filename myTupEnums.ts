2f23288a1b727c21ee80bf598e48209f0ec08f61
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-10-09 11:34:16 +0530

    tuples

---
+++ myTupEnums.ts
@@ -0,0 +1,25 @@
+const user :(string | number)[] = [1,"hc",]
+let tUser : [string, number, boolean]
+
+tUser = ["hc",131, true] // order of array elements need to be correct in tuple
+
+let rgb : [number,number,number] =[255,123,112]
+
+type User =[number,string]
+
+const newUser:User=[112,"example@google.com"]
+
+newUser[1] ="hc.com"
+newUser.push(true)
+
+
+
+
+
+
+
+
+
+
+
+export {}
